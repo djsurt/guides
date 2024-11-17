adb332808dd7a7be3047a754d2006bda7add2433
modelearth <model@dreamstudio.com>
2024-11-17 15:54:40 -0500

    Set initi value
    Click button twice

--- calculator.js
+++ calculator.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     let chart;
-    
     $('#calculatorForm').submit(function(e) {
+        generateGraph(e);
+    });
+    //generateGraph();
+    function generateGraph(e) {
         e.preventDefault();
         let equations = $('#equations').val().split(','); // Split equations by comma
         let minX = parseFloat($('#minX').val());
@@ -89,60 +92,61 @@ $(document).ready(function() {
         });
 
         $('.graphic-container').addClass('active');
-    });
+    }
 
     $('#resetBtn').on('click', () => {
         if (chart) chart.destroy();
         $('.graphic-container').removeClass('active');
     });
+
+
+    function getRandomColor() {
+        return '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
+
+    // Dark Mode and Light Mode theme toggler
+    document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
+    const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+    const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
+
+    var theme = "dark";
+    const root = document.querySelector(":root");
+    const container = document.getElementsByClassName("theme-container")[0];
+    const themeIcon = document.getElementById("theme-icon");
+    //container.addEventListener("click", setTheme);
+    function setTheme() {
+      switch (theme) {
+        case "dark":
+          setLight();
+          theme = "light";
+          break;
+        case "light":
+          setDark();
+          theme = "dark";
+          break;
+      }
+    }
+    function setLight() {
+      root.style.setProperty(
+        "--bs-dark",
+        "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
+      );
+      container.classList.remove("shadow-dark");
+      setTimeout(() => {
+        container.classList.add("shadow-light");
+        themeIcon.classList.remove("change");
+      }, 300);
+      themeIcon.classList.add("change");
+      themeIcon.src = sun;
+    }
+    function setDark() {
+      root.style.setProperty("--bs-dark", "#000");
+      container.classList.remove("shadow-light");
+      setTimeout(() => {
+        container.classList.add("shadow-dark");
+        themeIcon.classList.remove("change");
+      }, 300);
+      themeIcon.classList.add("change");
+      themeIcon.src = moon;
+    }
 });
-
-function getRandomColor() {
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
-}
-
-// Dark Mode and Light Mode theme toggler
-document.body.style="background-color: var(--bs-dark);transition: 0.5s;"
-const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
-const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg"
-
-var theme = "dark";
-const root = document.querySelector(":root");
-const container = document.getElementsByClassName("theme-container")[0];
-const themeIcon = document.getElementById("theme-icon");
-container.addEventListener("click", setTheme);
-function setTheme() {
-  switch (theme) {
-    case "dark":
-      setLight();
-      theme = "light";
-      break;
-    case "light":
-      setDark();
-      theme = "dark";
-      break;
-  }
-}
-function setLight() {
-  root.style.setProperty(
-    "--bs-dark",
-    "linear-gradient(318.32deg, #c3d1e4 0%, #dde7f3 55%, #d4e0ed 100%)"
-  );
-  container.classList.remove("shadow-dark");
-  setTimeout(() => {
-    container.classList.add("shadow-light");
-    themeIcon.classList.remove("change");
-  }, 300);
-  themeIcon.classList.add("change");
-  themeIcon.src = sun;
-}
-function setDark() {
-  root.style.setProperty("--bs-dark", "#000");
-  container.classList.remove("shadow-light");
-  setTimeout(() => {
-    container.classList.add("shadow-dark");
-    themeIcon.classList.remove("change");
-  }, 300);
-  themeIcon.classList.add("change");
-  themeIcon.src = moon;
-}
